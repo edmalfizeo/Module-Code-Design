e20203e1fcbd77efd916a7927af3cd809e75c226
edmalfizeo <edmalfizeo@gmail.com>
2024-11-30 20:16:18 -0300

    Creating factories for Calculators and Implementing Factories on Route

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,19 +1,17 @@
 from flask import Blueprint, request, jsonify
-from src.calculators.calculator_1 import Calculator1
-from src.calculators.calculator_2 import Calculator2
-from src.drivers.numpy_handler import NumpyHandler
+from src.main.factories.calculator_1_factory import calculator_1_factory
+from src.main.factories.calculator_2_factory import calculator_2_factory
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    calculator = Calculator1()
+    calculator = calculator_1_factory()
     response = calculator.calculate(request)
     return jsonify({"success": True, "message": response}), 200
 
 @calc_route_bp.route("/calculator/2", methods=["POST"])
 def calculator_2():
-    numpy_handler = NumpyHandler()
-    calculator = Calculator2(numpy_handler)
+    calculator = calculator_2_factory()
     response = calculator.calculate(request)
     return jsonify({"success": True, "message": response}), 200
