edd08c31312160dad85d0a206c26ef94dac641e6
edmalfizeo <edmalfizeo@gmail.com>
2024-11-30 20:01:55 -0300

    Creating Calculator 2 Route

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,10 +1,19 @@
 from flask import Blueprint, request, jsonify
 from src.calculators.calculator_1 import Calculator1
+from src.calculators.calculator_2 import Calculator2
+from src.drivers.numpy_handler import NumpyHandler
 
 calc_route_bp = Blueprint("calc_routes", __name__)
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
     calculator = Calculator1()
+    response = calculator.calculate(request)
+    return jsonify({"success": True, "message": response}), 200
+
+@calc_route_bp.route("/calculator/2", methods=["POST"])
+def calculator_2():
+    numpy_handler = NumpyHandler()
+    calculator = Calculator2(numpy_handler)
     response = calculator.calculate(request)
     return jsonify({"success": True, "message": response}), 200
